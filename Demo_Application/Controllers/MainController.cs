bdbfa2b3b0bb60fa00357157381ebab09568ea49
Yamini Sharma (BLR GSS) <yaminis@nousinfo.com>
2022-09-07 11:46:32 +0530

    first commit

---
+++ Demo_Application/Controllers/MainController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo_Application.Models;
+
+namespace Demo_Application.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        //public ActionResult Index()
+        public ActionResult Random()
+        {
+
+            var main = new Main() { Name = "Hello...!!" };
+                return View(main);
+        }
+    }
+}
---
+++ Demo_Application/Models/Main.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_Application.Models
+{
+    public class Main
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
